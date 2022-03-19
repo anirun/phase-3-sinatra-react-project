84c2b8f4f95dfe9c36a99abaec34ed58a26ef78a
Nurina Junus <nurina.junus@gmail.com>
2022-03-18 20:38:32 -0600

    added routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,50 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/assignments" do
+    assignments = Assignment.all.order(created_at: :asc)
+    assignments.to_json
+  end
+
+  post "/assignments/" do
+    assignment = Assignment.create(
+      name: params[:name],
+      teacher: params[:teacher],
+      student: params[:student]
+    )
+  end
+
+  patch "assignments/:id" do
+    assignment = Assignment.find(params[:id])
+    assignment.update(
+      completed: params[:completed],
+      graded: params[:grade],
+      grade: params[:grade]
+    )
+
+  get "/students" do
+    students = Student.all.order(name: :asc)
+    students.to_json
+  end
+
+  get "/students/:id" do
+    student = Student.find(params[:id])
+    student.to_json
+  end
+
+  patch "/students/:id" do
+    student = Student.find(params[:id])
+    student.update(
+      name: params[:name],
+      grade: params[:grade]
+    )
+    student.to_json
+  end
+
+  delete "students/:id" do
+    student = Student.find(params[:id])
+    student.destroy
+    student.to_json
   end
 
 end
